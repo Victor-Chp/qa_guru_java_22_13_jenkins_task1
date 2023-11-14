3ddc7206193b0e158daa1ca37eb0cc74dfd23fd3
Victor Chp <qatester906@gmail.com>
2023-11-14 14:33:07 +0300

    Add allure steps for StudentRegistrationFormRemoteTests

--- src/test/java/tests/StudentRegistrationFormRemoteTests.java
+++ src/test/java/tests/StudentRegistrationFormRemoteTests.java
@@ -48,43 +48,53 @@ public class StudentRegistrationFormRemoteTests extends RemoteTestBase {
     @Test
     @DisplayName("Successful Registration - Fill Required Fields")
     void successfulRegistrationFillRequiredFieldsTest() {
-
-        registrationFormPage.openPage()
-                .setFirstName(testData.firstName)
-                .setLastName(testData.lastName)
-                .setEmail(testData.userEmail)
-                .setGender(testData.userGender)
-                .setUserNumber(testData.userPhone)
-                .clickSubmit();
-
-        registrationFormPage.checkResultModalTable("Student Name", testData.firstName + " " + testData.lastName)
-                .checkResultModalTable("Student Email", testData.userEmail)
-                .checkResultModalTable("Gender", testData.userGender)
-                .checkResultModalTable("Mobile", testData.userPhone);
+        step("Open form", () -> {
+            registrationFormPage.openPage();
+        });
+        step("Fill form", () -> {
+            registrationFormPage.setFirstName(testData.firstName)
+                    .setLastName(testData.lastName)
+                    .setEmail(testData.userEmail)
+                    .setGender(testData.userGender)
+                    .setUserNumber(testData.userPhone)
+                    .clickSubmit();
+        });
+        step("Verify results", () -> {
+            registrationFormPage.checkResultModalTable("Student Name", testData.firstName + " " + testData.lastName)
+                    .checkResultModalTable("Student Email", testData.userEmail)
+                    .checkResultModalTable("Gender", testData.userGender)
+                    .checkResultModalTable("Mobile", testData.userPhone);
+        });
     }
 
     @Test
     @DisplayName("Unsuccessful Registration - Fill Wrong Email")
     void unsuccessfulRegistrationFillWrongEmailTest() {
-
-        registrationFormPage.openPage()
-                .setFirstName(testData.firstName)
-                .setLastName(testData.lastName)
-                .setEmail(testData.userWrongEmail)
-                .setGender(testData.userGender)
-                .setUserNumber(testData.userPhone)
-                .clickSubmit();
-
-        registrationFormPage.checkModalDialogHidden();
+        step("Open form", () -> {
+            registrationFormPage.openPage();
+        });
+        step("Fill form", () -> {
+            registrationFormPage.setFirstName(testData.firstName)
+                    .setLastName(testData.lastName)
+                    .setEmail(testData.userWrongEmail)
+                    .setGender(testData.userGender)
+                    .setUserNumber(testData.userPhone)
+                    .clickSubmit();
+        });
+        step("Verify results", () -> {
+            registrationFormPage.checkModalDialogHidden();
+        });
     }
 
     @Test
     @DisplayName("Unsuccessful Registration - Not Fill Form")
     void unsuccessfulRegistrationNotFillFormTest() {
-
-        registrationFormPage.openPage()
-                .clickSubmit();
-
-        registrationFormPage.checkModalDialogHidden();
+        step("Open form and click Submit", () -> {
+            registrationFormPage.openPage()
+                    .clickSubmit();
+        });
+        step("Modal dialog should be hidden", () -> {
+            registrationFormPage.checkModalDialogHidden();
+        });
     }
 }
